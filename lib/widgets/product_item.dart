f0352a50be6a01a99e3571bd1c5411036ae6a334
Viweld <litvar@outlook.com>
2021-06-25 17:59:39 +0300

    first commit

---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:myshop/providers/product.dart';
+import 'package:myshop/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+
+class ProductItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
+    /*return Consumer<Product>(
+      builder: (ctx, product, child) {*/
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+          backgroundColor: Colors.black87,
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) {
+              return IconButton(
+                icon: Icon(product.isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                color: Theme.of(context).accentColor,
+                onPressed: () {
+                  product.toggleFavoriteStatus();
+                },
+              );
+            },
+          ),
+          trailing: IconButton(
+            icon: Icon(Icons.add_shopping_cart),
+            color: Theme.of(context).accentColor,
+            onPressed: () {},
+          ),
+          title: Text(
+            product.title,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
+}
